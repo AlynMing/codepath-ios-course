d446b4e11c2f898f44203f8c773f185894d269b5
CleverChuk <chubiyke@gmail.com>
2020-09-21 22:43:50 -0400

    unit 1

---
+++ Flix/Flix/MovieTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieTableViewController.swift
+//  Flix
+//
+//  Created by Chukwubuikem Ume-Ugwa on 9/20/20.
+//
+
+import UIKit
+
+class MovieTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
