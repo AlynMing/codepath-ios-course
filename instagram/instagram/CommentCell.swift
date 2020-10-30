270eab0738375d3171d906435722a39dbf9c6505
CleverChuk <chubiyke@gmail.com>
2020-10-29 20:02:03 -0400

    second pass

---
+++ instagram/instagram/CommentCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommentCell.swift
+//  instagram
+//
+//  Created by Chukwubuikem Ume-Ugwa on 10/29/20.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var comment: UILabel!
+    @IBOutlet weak var username: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- instagram/instagram/FeedViewController.swift
+++ instagram/instagram/FeedViewController.swift
@@ -8,22 +8,68 @@
 import UIKit
 import Parse
 import AlamofireImage
+import MessageInputBar
 
-class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDataSource, MessageInputBarDelegate{
     var posts = [PFObject]()
     @IBOutlet weak var tableView: UITableView!
+    let commentBar = MessageInputBar()
+    var showsCommentBar = false
+    
+    var selectedPost: PFObject!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         // Do any additional setup after loading the view.
+        tableView.keyboardDismissMode = .interactive
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(hideKeyBoard(note:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        commentBar.inputTextView.placeholder = "Add a comment ..."
+        commentBar.sendButton.title = "Post"
+        commentBar.delegate = self
+    }
+    
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        commentBar.inputTextView.text = nil
+        showsCommentBar = false
+        becomeFirstResponder()
+        commentBar.inputTextView.resignFirstResponder()
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = text
+        comment["author"] = PFUser.current()!
+        comment["post"] = selectedPost
+        selectedPost.add(comment, forKey: "comments")
+        selectedPost.saveInBackground{
+            (success, error) in
+            if success {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    @objc func hideKeyBoard(note: Notification){
+        commentBar.inputTextView.text = nil
+        showsCommentBar = false
+        becomeFirstResponder()
+        commentBar.inputTextView.resignFirstResponder()
+    }
+    
+    override var inputAccessoryView: UIView?{
+        return commentBar
+    }
+    
+    override var canBecomeFirstResponder: Bool{
+        return showsCommentBar
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = 20
         
         query.findObjectsInBackground { (posts, error) in
@@ -34,26 +80,62 @@ class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDat
         }
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count;
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        return comments.count + 2;
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as! PostCell
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        let post = posts[indexPath.row]
-        let user = post["author"] as! PFUser
-        cell.username.text = user["username"] as? String
-        cell.caption.text = post["caption"] as? String
-        
-        let imageFile = post["image"] as! PFFileObject
-        let url = URL(string: imageFile.url!)
-        
-        cell.photoView.af.setImage(withURL: url!)
-        return cell
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as! PostCell
+            
+
+            let user = post["author"] as! PFUser
+            cell.username.text = user["username"] as? String
+            cell.caption.text = post["caption"] as? String
+            
+            let imageFile = post["image"] as! PFFileObject
+            let url = URL(string: imageFile.url!)
+            
+            cell.photoView.af.setImage(withURL: url!)
+            return cell
+            
+        }else if indexPath.row <= comments.count && comments.count > 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell") as! CommentCell
+                        
+            let comment = comments[indexPath.row - 1]
+            let user = comment["author"] as! PFUser
+            cell.username.text = user["username"] as? String
+            cell.comment.text = comment["text"] as? String
+            return cell
+            
+        }else{
+            return  tableView.dequeueReusableCell(withIdentifier: "addCommentCell")!
+        }
     }
     
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.section]
+        
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        if indexPath.row == comments.count + 1 {
+            selectedPost = post
+            showsCommentBar = true
+            becomeFirstResponder()
+            commentBar.inputTextView.becomeFirstResponder()
+        }
+    }
 
     
     @IBAction func onLogOut(_ sender: Any) {
